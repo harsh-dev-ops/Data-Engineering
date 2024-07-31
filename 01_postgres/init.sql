af11a4a1fb03226dcc27922205c107dd1518b0e4
Harsh Singh <harshkushwah2011@gmail.com>
2024-07-31 12:54:59 +0530

    fixed init.sql

--- 01_postgres/init.sql
+++ 01_postgres/init.sql
@@ -2,4 +2,9 @@ CREATE DATABASE retail_db;
 
 CREATE USER retail_user WITH ENCRYPTED PASSWORD 'Retail123';
 
-GRANT ALL ON DATABASE retail_db TO retail_user;
+ALTER ROLE retail_user SET client_encoding TO 'utf8';
+ALTER ROLE retail_user SET default_transaction_isolation TO 'read committed';
+ALTER ROLE retail_user SET timezone TO 'Asia/Kolkata';
+GRANT ALL PRIVILEGES ON DATABASE retail_db TO retail_user;
+\c retail_db
+GRANT ALL ON SCHEMA public TO retail_user;
