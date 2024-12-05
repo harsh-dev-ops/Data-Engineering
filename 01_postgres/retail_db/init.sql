4e1f77d835a805539f8a2949b1353c783a7b5140
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-05 16:49:41 +0530

    update - modified user creation for postgres database

--- 01_postgres/retail_db/init.sql
+++ 01_postgres/retail_db/init.sql
@@ -5,6 +5,11 @@ CREATE USER retail_user WITH ENCRYPTED PASSWORD 'Retail123';
 ALTER ROLE retail_user SET client_encoding TO 'utf8';
 ALTER ROLE retail_user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE retail_user SET timezone TO 'Asia/Kolkata';
-GRANT ALL PRIVILEGES ON DATABASE retail_db TO retail_user;
-\c retail_db
-GRANT ALL ON SCHEMA public TO retail_user;
+GRANT ALL ON DATABASE retail_db TO retail_user;
+ALTER DATABASE retail_db OWNER TO retail_user;
+
+-- GRANT ALL PRIVILEGES ON DATABASE retail_db TO retail_user;
+-- GRANT ALL PRIVILEGES ON SCHEMA public TO retail_user;
+-- -- \c retail_db postgres
+-- GRANT ALL ON SCHEMA public TO retail_user;
+-- GRANT USAGE ON SCHEMA public TO retail_user;
