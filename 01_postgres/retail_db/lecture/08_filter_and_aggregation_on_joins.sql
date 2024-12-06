e363cbba90d00c63b6d09fe437d5862e4f5f5706
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-06 19:09:57 +0530

    fixed typo

--- 01_postgres/retail_db/lecture/08_filter_and_aggregation_on_joins.sql
+++ 01_postgres/retail_db/lecture/08_filter_and_aggregation_on_joins.sql
@@ -5,6 +5,6 @@ round(sum(oi.order_item_subtotal)::numeric, 2) AS order_revenue
 FROM orders AS o
     JOIN order_items AS oi
         ON o.order_id = oi.order_item_order_id
-WHERE o.order_status IN ('COMPLETED', 'CLOSED')
+WHERE o.order_status IN ('COMPLETE', 'CLOSED')
 GROUP BY 1, 2
 ORDER BY 1, 3 DESC;
