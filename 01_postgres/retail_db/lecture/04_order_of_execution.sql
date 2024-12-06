17fc537845988dff5a3f081b54b10901b0f44ae2
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-06 15:14:40 +0530

    rules related to writing sql queries

--- 01_postgres/retail_db/lecture/04_order_of_execution.sql
+++ 01_postgres/retail_db/lecture/04_order_of_execution.sql
@@ -9,3 +9,16 @@ FROM orders
 WHERE order_status IN ('COMPLETE', 'CLOSED')
 GROUP BY 1
 ORDER BY 2 DESC;
+
+
+-- Rules related to execution of sql queries
+
+-- Aggregate function
+-- can't be excecuted with  WHERE & GROUP BY
+-- can be use with ORDER BY 
+
+SELECT order_date, count(*) AS order_count
+FROM orders
+WHERE order_status IN ('COMPLETE', 'CLOSED')
+GROUP BY 1
+ORDER BY count(*) DESC;
