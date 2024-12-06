f873a7ce137680274f957cd45bd8e2532b4bb613
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-06 23:09:11 +0530

    added - outer join with additional conditions

---
+++ 01_postgres/lectures/01_basic_sql_queries/11_outer_join_with_additional_conditions.sql
@@ -0,0 +1,38 @@
+SELECT * FROM order_details_v;
+
+SELECT * FROM products;
+
+SELECT *
+FROM products AS p
+	LEFT OUTER JOIN order_details_v AS odv
+		ON p.product_id = odv.order_item_product_id;
+
+
+SELECT *
+FROM products AS p
+	LEFT OUTER JOIN order_details_v AS odv
+		ON p.product_id = odv.order_item_product_id
+WHERE odv.order_item_product_id is NULL;
+
+
+-- Select the products which are not sold on 2014-01
+
+-- WRONG, return Empty
+
+SELECT *
+FROM products AS p
+	LEFT OUTER JOIN order_details_v AS odv
+		ON p.product_id = odv.order_item_product_id
+WHERE to_char(odv.order_date::timestamp, 'yyyy-MM') = '2014-01'
+	AND odv.order_item_product_id is NULL;
+
+
+-- RIGHT
+SELECT *
+FROM products AS p
+	LEFT OUTER JOIN order_details_v AS odv
+		ON p.product_id = odv.order_item_product_id
+			AND to_char(odv.order_date, 'yyyy-MM') = '2014-01'
+WHERE odv.order_item_product_id is NULL;
+
+	
