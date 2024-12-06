926c2955098db135f1e0ba3ec21979e093f55899
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-06 20:10:14 +0530

    added - cte

---
+++ 01_postgres/lectures/01_basic_sql_queries/10_common_table_expressions_(CTE).sql
@@ -0,0 +1,14 @@
+WITH order_details_cte
+AS
+(
+    SELECT 
+    o.*,
+    oi.order_item_product_id,
+    oi.order_item_subtotal,
+    oi.order_item_order_id
+FROM orders AS o
+    JOIN order_items AS oi
+        ON o.order_id  = oi.order_item_order_id
+)
+
+SELECT * FROM order_details_cte;
