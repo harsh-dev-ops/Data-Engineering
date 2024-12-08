b5b1e90cc807331046c9d5f90762a9880134ebe1
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-08 20:31:16 +0530

    added - daily_revenue and daily_product_revenue tables

---
+++ postgresql/lectures/02_cumulative_aggrregation_and_ranking/02_create_tables_for_cummulative_aggregations.sql
@@ -0,0 +1,29 @@
+-- daily revenue
+CREATE TABLE daily_revenue
+AS
+SELECT o.order_date,
+	round(sum(oi.order_item_subtotal)::numeric, 2) as order_revenue
+FROM orders AS o
+    JOIN order_items AS oi
+        ON o.order_id = oi.order_item_order_id
+WHERE o.order_status IN ('COMPLETE', 'CLOSED')
+GROUP BY 1;
+
+SELECT * FROM daily_revenue
+ORDER BY order_date;
+
+
+-- daily product revenue
+CREATE TABLE daily_product_revenue
+AS
+SELECT o.order_id,
+		oi.order_item_product_id,
+		round(sum(oi.order_item_subtotal)::numeric, 2) AS order_revenue
+FROM orders AS o
+	JOIN order_items AS oi
+		ON o.order_id = oi.order_item_order_id
+WHERE o.order_status IN ('COMPLETE', 'CLOSED')
+GROUP BY 1, 2;
+
+SELECT * FROM daily_product_revenue
+ORDER BY 1, 3 DESC;
