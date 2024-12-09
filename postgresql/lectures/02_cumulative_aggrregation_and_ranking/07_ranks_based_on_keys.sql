42d82804d50e434dafab15c7c287725d24328966
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-09 16:30:29 +0530

    clean code

--- postgresql/lectures/02_cumulative_aggrregation_and_ranking/07_ranks_based_on_keys.sql
+++ postgresql/lectures/02_cumulative_aggrregation_and_ranking/07_ranks_based_on_keys.sql
@@ -5,11 +5,11 @@ SELECT order_date,
 		order_revenue,
 		rank() OVER (
 			PARTITION BY order_date 
-				ORDER BY order_revenue DESC
+			ORDER BY order_revenue DESC
 		),
 		dense_rank() OVER (
 			PARTITION BY order_date
-				ORDER BY order_revenue DESC
+			ORDER BY order_revenue DESC
 		)
 FROM daily_product_revenue AS dr
 WHERE to_char(order_date::date, 'yyyy-MM') = '2014-01'
