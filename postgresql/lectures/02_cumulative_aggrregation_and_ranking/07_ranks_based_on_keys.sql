e6cb514eb8bb3a56988677beb39c8de5dec5bfd4
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-09 16:21:00 +0530

    ranks based on sql queries

---
+++ postgresql/lectures/02_cumulative_aggrregation_and_ranking/07_ranks_based_on_keys.sql
@@ -0,0 +1,16 @@
+SELECT * FROM daily_product_revenue;
+
+SELECT order_date,
+		order_item_product_id,
+		order_revenue,
+		rank() OVER (
+			PARTITION BY order_date 
+				ORDER BY order_revenue DESC
+		),
+		dense_rank() OVER (
+			PARTITION BY order_date
+				ORDER BY order_revenue DESC
+		)
+FROM daily_product_revenue AS dr
+WHERE to_char(order_date::date, 'yyyy-MM') = '2014-01'
+ORDER BY 1, 3 DESC;
