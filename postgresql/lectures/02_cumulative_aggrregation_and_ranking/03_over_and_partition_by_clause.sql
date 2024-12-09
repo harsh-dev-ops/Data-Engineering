72ff7ec75e480ed201ae9ba900a8dff718794eaf
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-09 12:08:55 +0530

    added - over and partition by clause

---
+++ postgresql/lectures/02_cumulative_aggrregation_and_ranking/03_over_and_partition_by_clause.sql
@@ -0,0 +1,12 @@
+SELECT * FROM daily_revenue;
+
+SELECT 
+to_char(dr.order_date::timestamp, 'yyyy-MM') as order_month,
+dr.order_date,
+dr.order_revenue,
+sum(order_revenue) OVER (
+	PARTITION BY to_char(dr.order_date::timestamp, 'yyyy-MM')
+		)
+AS monthly_order_revenue 
+FROM daily_revenue AS dr
+ORDER BY 2;
