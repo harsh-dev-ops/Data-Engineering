fb5e2fe27fa06a0e86f2cbda329bbcb712bd6d9a
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-09 13:21:35 +0530

    fixed daily revenue table error

--- postgresql/lectures/02_cumulative_aggrregation_and_ranking/02_create_tables_for_cummulative_aggregations.sql
+++ postgresql/lectures/02_cumulative_aggrregation_and_ranking/02_create_tables_for_cummulative_aggregations.sql
@@ -18,15 +18,15 @@ DROP TABLE daily_product_revenue;
 
 CREATE TABLE daily_product_revenue
 AS
-SELECT o.order_id,
-		oi.order_item_product_id,
-		round(sum(oi.order_item_subtotal)::numeric, 2) AS order_revenue,
-		o.order_date
+SELECT o.order_date,
+	oi.order_item_product_id,
+	round(sum(oi.order_item_subtotal)::numeric, 2) AS order_revenue
 FROM orders AS o
 	JOIN order_items AS oi
 		ON o.order_id = oi.order_item_order_id
 WHERE o.order_status IN ('COMPLETE', 'CLOSED')
 GROUP BY 1, 2;
+
 
 SELECT * FROM daily_product_revenue
 ORDER BY 1, 3 DESC;
