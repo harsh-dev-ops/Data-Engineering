016259748154ec8490b9b23c9cd2058d003253a4
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-22 08:23:17 +0530

    altered foreign keys

--- postgresql/lectures/04_performance_tunning/02_adding_foreign_keys.sql
+++ postgresql/lectures/04_performance_tunning/02_adding_foreign_keys.sql
@@ -3,3 +3,6 @@ DROP INDEX order_items_oid_idx;
 
 ALTER TABLE order_items ADD
 	FOREIGN KEY (order_item_order_id) REFERENCES orders (order_id);
+
+ALTER TABLE orders
+	ADD FOREIGN KEY (order_customer_id) REFERENCES customers (customer_id);
