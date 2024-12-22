752010d72551ddbea58715d63b89f135f715416e
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-22 11:23:40 +0530

    filter and aggergation excersice completed

---
+++ postgresql/lectures/05_excercise/02_aggregate_functions.sql
@@ -0,0 +1,45 @@
+-- Let us take care of exercises related to filtering and aggregations using SQL.
+SELECT * FROM courses;
+
+-- * Get all the details of the courses which are in `inactive` or `draft` state.
+SELECT course_name 
+FROM courses
+WHERE course_status IN ('inactive', 'draft');
+
+
+-- * Get all the details of the courses which are related to `Python` or `Scala`.
+SELECT * 
+FROM courses
+WHERE course_name LIKE '%Python%' OR course_name LIKE '%Scala%';
+
+
+-- * Get count of courses by `course_status`. The output should contain `course_status` and `course_count`.
+SELECT course_status, 
+		count(*) as course_count 
+FROM courses
+GROUP BY 1;
+
+
+-- * Get count of `published` courses by `course_author`. The output should contain `course_author` and `course_count`.
+SELECT course_author,
+	count(*) as course_count
+FROM courses
+WHERE course_status = 'published'
+GROUP BY 1;
+
+
+-- * Get all the details of `Python` or `Scala` related courses in `draft` status.
+SELECT * 
+FROM courses
+WHERE (
+		course_name LIKE '%Python%' OR course_name LIKE '%Scala%'
+		) AND (course_status='draft');
+
+
+-- * Get the author and count where the author have more than **one published** course. The output should contain `course_author` and `course_count`.
+SELECT course_author,
+		count(*) AS course_count
+FROM courses
+WHERE course_status = 'published'
+GROUP BY 1
+	HAVING count(*) > 1;
